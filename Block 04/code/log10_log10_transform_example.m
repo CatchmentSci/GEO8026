b0e1ca909dfec6d2443cc52387a9c4cce831a876
catchmentSci <matthew.perks@ncl.ac.uk>
2022-09-12 23:59:52 +0100

    updates for AY 22/23

--- Block 04/code/log10_log10_transform_example.m
+++ Block 04/code/log10_log10_transform_example.m
@@ -1,4 +1,5 @@
-
+% create some data where yObs increase slowly to changes in xObs when xObs 
+% is small, and then the rate of change increases as the value of xObs increases. 
 clear XData_mod YData_mod
 yData = randperm(1000,200);
 xData = sqrt(yData); % generate random x-axis data
@@ -12,32 +13,33 @@ xData(1:t_out,2)    = 1; % Prep the data for regression with a column of ones
 Original_r2         = stats(1,1); % Pull out the R2 value
 Original_p          = stats(1,3); % Pull out the p value
 
-
 %Let's plot the results:
-ax1       = subplot(2,2,1); hold on;
-set(gcf,'units','normalized','outerposition',[0 0 0.5 1]); 
+f3 = figure(); hold on;
+ax1_3     = subplot(2,2,1); hold on;
+set(f3,'units','normalized','outerposition',[0 0 0.5 1]); 
 h(1)      = scatter(xData(:,1),yData,... % we need to specify only the column due to the column of ones
     'MarkerEdgeColor', [0.5,0.5,0.5],...
     'MarkerFaceColor', [0.5,0.5,0.5],...
     'SizeData', 10);
 
 intervals   = min(xData):0.01:max(xData);  % we need to specify the range of values to calculate the fit for.
-h(2)          = plot(intervals,b(2) + b(1).*intervals,'k-'); % Plot the best-fit: b1 = slope, b2 = intecept
+h(2)        = plot(intervals,b(2) + b(1).*intervals,'k-'); % Plot the best-fit: b1 = slope, b2 = intecept
 
-ylabel('yObs','FontSize', 14);
-xlabel('xObs','FontSize',14);
+ylabel('y Obs','FontSize', 14);
+xlabel('x Obs','FontSize',14);
 h_leg(1) = legend(h(2), ['y = ' num2str(b(2)) '+' num2str(b(1)) 'x']);
 legend boxoff ;
 set(h_leg(1),'Location','best');
 
 % Let's examine the residuals
 % The residual vs. fit plot can be used to detect non-linearity and/or unequal variance.
-ax2     = subplot(2,2,2); hold on
-h(3)    = scatter(yData,r,... % plot the fit vs residuals
+fitted  = b(2) + b(1).*xData(:,1); % caluclate the fitted estimate for each xObs
+ax2_3   = subplot(2,2,2); hold on
+h(3)    = scatter(fitted,r,... % plot the fit vs residuals
     'MarkerEdgeColor', [0.5,0.5,0.5],...
     'MarkerFaceColor', [0.5,0.5,0.5],...
     'SizeData', 10);
-xlabel('yObs','FontSize', 14);
+xlabel('y predicted','FontSize', 14);
 ylabel('residuals','FontSize', 14);
 
 XData_mod               = log10(xData(:,1));
@@ -49,18 +51,18 @@ Original_r2             = stats(1,1); % Pull out the R2 value
 Original_p              = stats(1,3); % Pull out the p value
 
 %Let's plot the results:
-ax3     = subplot(2,2,3); hold on
+ax3_3   = subplot(2,2,3); hold on
 h(4)    = scatter(XData_mod(:,1),yData_mod,... % we need to specify only the column due to the column of ones
     'MarkerEdgeColor', [0.5,0.5,0.5],...
     'MarkerFaceColor', [0.5,0.5,0.5],...
     'SizeData', 10);
-ylabel('yObs','FontSize', 14);
-xlabel('xObs','FontSize',14);
+ylabel('y Obs','FontSize', 14);
+xlabel('x Obs','FontSize',14);
 
 intervals   = min(XData_mod):0.01:max(XData_mod);  % we need to specify the range of values to calculate the fit for.
 h(5)        = plot(intervals,b(2) + b(1).*intervals,'k-'); % Plot the best-fit: b1 = slope, b2 = intecept
-ylabel('log10(yObs)','FontSize', 14);
-xlabel('log10(xObs)','FontSize', 14);
+ylabel('log10(y Obs)','FontSize', 14);
+xlabel('log10(x Obs)','FontSize', 14);
 
 h_leg(2) = legend(h(5), ['log10(y) = ' num2str(b(2)) '+' num2str(b(1)) ' log10(xObs)' newline ...
     'y = 10^{' num2str(b(2)) ' + ' num2str(b(1)) ' log10(x)}']);
@@ -69,10 +71,11 @@ set(h_leg(2),'Location','best');
 
 % Let's examine the residuals
 % The residual vs. fit plot can be used to detect non-linearity and/or unequal variance.
-ax4     = subplot(2,2,4); hold on
-h(6)    = scatter(yData_mod,r,... % plot the fit vs residuals
+fitted  = b(2) + b(1).*XData_mod(:,1); % caluclate the fitted estimate for each xObs
+ax4_3   = subplot(2,2,4); hold on
+h(6)    = scatter(fitted,r,... % plot the fit vs residuals
     'MarkerEdgeColor', [0.5,0.5,0.5],...
     'MarkerFaceColor', [0.5,0.5,0.5],...
     'SizeData', 10);
-xlabel('yObs','FontSize', 14);
+xlabel('y predicted','FontSize', 14);
 ylabel('residuals','FontSize', 14);
--- Block 04/code/sqrt_transform_example.m
+++ Block 04/code/sqrt_transform_example.m
@@ -1,11 +1,9 @@
-% the rate of change decreases slightly
 % Generate some data which increases quite rapidly at the start and then
 % the rate of change decreases slightly
 clear XData_mod YData_mod
 xData = randperm(1000,200);
 yData = sqrt(xData).*10; % generate random x-axis data
 yData = awgn( yData , 25, 'measured' );
-scatter(xData,yData)
 
 % Run a linear regression
 [xData,yData]       = prepareCurveData(xData,yData); % checks for and removes NaN
@@ -17,30 +15,32 @@ Original_p          = stats(1,3); % Pull out the p value
 
 
 %Let's plot the results:
-ax1       = subplot(2,2,1); hold on;
-set(gcf,'units','normalized','outerposition',[0 0 0.5 1]); 
+f2 = figure(); hold on;
+ax1_2     = subplot(2,2,1); hold on;
+set(f2,'units','normalized','outerposition',[0 0 0.5 1]); 
 h(1)      = scatter(xData(:,1),yData,... % we need to specify only the column due to the column of ones
     'MarkerEdgeColor', [0.5,0.5,0.5],...
     'MarkerFaceColor', [0.5,0.5,0.5],...
     'SizeData', 10);
 
 intervals   = min(xData):0.01:max(xData);  % we need to specify the range of values to calculate the fit for.
-h(2)          = plot(intervals,b(2) + b(1).*intervals,'k-'); % Plot the best-fit: b1 = slope, b2 = intecept
+h(2)        = plot(intervals,b(2) + b(1).*intervals,'k-'); % Plot the best-fit: b1 = slope, b2 = intecept
 
-ylabel('yObs','FontSize', 14);
-xlabel('xObs','FontSize',14);
+ylabel('y Obs','FontSize', 14);
+xlabel('x Obs','FontSize',14);
 h_leg(1) = legend(h(2), ['y = ' num2str(b(2)) '+' num2str(b(1)) 'x']);
 legend boxoff ;
 set(h_leg(1),'Location','best');
 
 % Let's examine the residuals
 % The residual vs. fit plot can be used to detect non-linearity and/or unequal variance.
-ax2     = subplot(2,2,2); hold on
-h(3)    = scatter(yData,r,... % plot the fit vs residuals
+fitted  = b(2) + b(1).*xData(:,1); % caluclate the fitted estimate for each xObs
+ax2_2   = subplot(2,2,2); hold on
+h(3)    = scatter(fitted,r,... % plot the fit vs residuals
     'MarkerEdgeColor', [0.5,0.5,0.5],...
     'MarkerFaceColor', [0.5,0.5,0.5],...
     'SizeData', 10);
-xlabel('yObs','FontSize', 14);
+xlabel('y predicted','FontSize', 14);
 ylabel('residuals','FontSize', 14);
 
 XData_mod               = sqrt(xData(:,1));
@@ -51,18 +51,18 @@ Original_r2             = stats(1,1); % Pull out the R2 value
 Original_p              = stats(1,3); % Pull out the p value
 
 %Let's plot the results:
-ax3     = subplot(2,2,3); hold on
+ax3_2   = subplot(2,2,3); hold on
 h(4)    = scatter(XData_mod(:,1),yData,... % we need to specify only the column due to the column of ones
     'MarkerEdgeColor', [0.5,0.5,0.5],...
     'MarkerFaceColor', [0.5,0.5,0.5],...
     'SizeData', 10);
-ylabel('yObs','FontSize', 14);
-xlabel('xObs','FontSize',14);
+ylabel('y Obs','FontSize', 14);
+xlabel('x Obs','FontSize',14);
 
 intervals   = min(XData_mod):0.01:max(XData_mod);  % we need to specify the range of values to calculate the fit for.
 h(5)        = plot(intervals,b(2) + b(1).*intervals,'k-'); % Plot the best-fit: b1 = slope, b2 = intecept
-ylabel('yObs','FontSize', 14);
-xlabel('sqrt(xObs)','FontSize', 14);
+ylabel('y Obs','FontSize', 14);
+xlabel('sqrt(x Obs)','FontSize', 14);
 
 h_leg(2) = legend(h(5), ['y = ' num2str(b(2)) '+' num2str(b(1)) 'sqrt(xObs)']);
 legend boxoff ;
@@ -70,10 +70,11 @@ set(h_leg(2),'Location','best');
 
 % Let's examine the residuals
 % The residual vs. fit plot can be used to detect non-linearity and/or unequal variance.
-ax4     = subplot(2,2,4); hold on
+fitted  = b(2) + b(1).*XData_mod(:,1); % caluclate the fitted estimate for each xObs
+ax4_2   = subplot(2,2,4); hold on
 h(6)    = scatter(yData,r,... % plot the fit vs residuals
     'MarkerEdgeColor', [0.5,0.5,0.5],...
     'MarkerFaceColor', [0.5,0.5,0.5],...
     'SizeData', 10);
-xlabel('yObs','FontSize', 14);
+xlabel('y predicted','FontSize', 14);
 ylabel('residuals','FontSize', 14);
